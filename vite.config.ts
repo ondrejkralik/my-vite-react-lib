2956a00ba098aa01d244e669e76084fe02ccf650
Ondřej Králík <ondrej.kralik@gmail.com>
2024-02-02 23:48:45 +0100

    Do not copy /public folder into /dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
+    copyPublicDir: false,
     lib: {
       entry: resolve(__dirname, "lib/main.ts"),
       formats: ["es"],
