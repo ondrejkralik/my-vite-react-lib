e29c5f01df861841a57dc631f233e8c6f6df1dc2
Ondřej Králík <ondrej.kralik@gmail.com>
2024-02-02 23:51:47 +0100

    generate lib components TS types

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ include: ["lib"] })],
   build: {
     copyPublicDir: false,
     lib: {
